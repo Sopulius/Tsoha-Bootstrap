cb0c796428c551d230f56ebb55dc9d9e6289aaaa
alsu <alsu@alsu-VirtualBox.(none)>
2016-05-30 14:11:05 +0300

    Lisätty suunnitelmat näkymistä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,32 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+         View::make('suunnitelmat/home.html');
+        
+    }
+    
+    public static function login(){
+        View::make('suunnitelmat/login.html');
+    }
+    
+    public static function section_show(){
+        View::make('suunnitelmat/section_show.html');
+    }
+    
+    public static function product_show(){
+        View::make('suunnitelmat/product_show.html');
+    }
+    
+    public static function product_edit(){
+        View::make('suunnitelmat/product_edit.html');
+    }
+    
+    public static function user_products_show(){
+        View::make('suunnitelmat/show_user_products.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,26 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/kayttajat/Pekka', function() {
+    HelloWorldController::user_products_show();
+  });
+  
+  $routes->get('/login', function() {
+   HelloWorldController::login();
+           
+  });
+  
+  $routes->get('osastot/musiikki', function() {
+   HelloWorldController::section_show();
+           
+  });
+   
+  $routes->get('osastot/musiikki/1', function() {
+   HelloWorldController::product_show();        
+  });
+  
+    
+  $routes->get('osastot/musiikki/1/edit', function() {
+   HelloWorldController::product_edit();       
+  });
